974bb6ff81bb90399cdfef525259a6f269ce6e94
Mathieu Saury <mathieu.saury@gmail.com>
2012-01-25 21:18:46 -0500

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathieusaury::Application.config.secret_token = '4db50b496deb3a65bd39277664318a776bf77746c8dd1293ec6c8124639ca6c6c9f42147b2f1dc88576f210250b20fa992a9a42ce6bd73b44d52e9e7ec86e78f'
